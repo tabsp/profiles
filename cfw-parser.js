4f910ff0182f491f89db3cd8750a269a74a98074
tabsp <tabsp@qq.com>
2023-10-15 21:01:15 +0800

    Update cfw-parser.js

--- cfw-parser.js
+++ cfw-parser.js
@@ -1,5 +1,6 @@
 module.exports.parse = async (raw, { axios, yaml, notify, console }) => {
     const obj = yaml.parse(raw)
     obj.rules.unshift("DOMAIN-SUFFIX,vercel.app,Proxies")
+    obj.rules.unshift("DOMAIN-SUFFIX,cloudflare.com,Proxies")
     return yaml.stringify(obj)
 }
